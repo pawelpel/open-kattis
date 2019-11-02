48c8a7d80fe5bcfac0baebfb70663b14bf299e06
Pawel Baranowski <p.baranowski@poradzis.pl>
2019-11-02 20:14:51 +0100

    Rewrting whole solution for BanQueue

--- BankQueue.py
+++ BankQueue.py
@@ -1,47 +1,29 @@
+import sys
 import bisect
-import operator
 
-N, BANK_TIME = map(int, input().split())
+N, T = map(int, sys.stdin.readline().replace('\n', '').split())
 
 que = {}
 
 for _ in range(N):
-    cash, time = map(int, input().split())
+    cash, time = sys.stdin.readline().replace('\n', '').split()
+    cash = int(cash)
     if time in que:
-        bisect.insort(que[time], cash)
+        que[time].append(cash)
     else:
         que[time] = [cash]
 
 cash = 0
+que_line = []
 
-for t in range(BANK_TIME):
-    keys = que.keys()
-    if not keys:
-        break
+for time in range(T, -1, -1):
+    time = str(time)
     
-    # print('T:', BANK_TIME-t)
-
-    que_comp = {}
-    for k, v in que.items():
-        v_len = len(v)
-        
-        if k in [0, 1] and v_len >= k + 1:
-            que_comp[k] = v[-k-1]
-        elif k > 1 and (v_len > k or v_len > BANK_TIME-t):
-            que_comp[k] = v[-k]
-
-    # print(que)
-    # print(que_comp)
-
-    if len(que_comp):
-        to_pop_key = sorted(que_comp.items(), key=operator.itemgetter(1), reverse=True)[0][0]
-    else:
-        to_pop_key = sorted(que.items(), key=operator.itemgetter(1), reverse=True)[0][0]
-        
-    # print('Pop:', que[to_pop_key][-1])
-
-    cash += que[to_pop_key].pop()
-
-    que = {k-1: v for k, v in que.items() if len(v) and k-1 >= 0}
+    if time in que:
+        for person in que[time]:
+            bisect.insort(que_line, person)
+    
+    if len(que_line):
+        cash += que_line.pop()
 
 print(cash)
